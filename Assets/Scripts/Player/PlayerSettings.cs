0ff1863b895722c0db017eecbab3a540b8152cca
Qiao Xu <sleeepd@gmail.com>
2025-05-09 02:21:27 -0600

    Add FreeLook Camera; Add basic player setting scriptable object

---
+++ Assets/Scripts/Player/PlayerSettings.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerSettings", menuName = "Scriptable Objects/PlayerSettings")]
+public class PlayerSettings : ScriptableObject
+{
+    public int maxHealth;
+    public float jumpForce;
+}
